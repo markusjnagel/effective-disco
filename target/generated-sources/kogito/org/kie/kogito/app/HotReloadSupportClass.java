253ebdee80750df4784549645ace483daca5d41a
Markus Nagel <mnagel@redhat.com>
2021-10-18 22:29:47 +0200

    test for bitbucket transfer

--- target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
+++ target/generated-sources/kogito/org/kie/kogito/app/HotReloadSupportClass.java
@@ -1,5 +1,5 @@
 package org.kie.kogito.app;
 @io.quarkus.runtime.Startup()
 public class HotReloadSupportClass {
-private static final String ID = "953762c7-8f64-4579-9e83-c6234d7babf6";
+private static final String ID = "27b71c75-af71-4d29-833a-9f649b4b7092";
 }
